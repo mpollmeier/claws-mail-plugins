09d2cae8fe061b48061a62da9e268367d1996871
reboot <reboot>
2004-07-30 16:12:00 +0000

    * src/viewer.c * src/viewer.glade         add "First Page" and "Last Page" to popup menu

--- ghostscript-viewer/src/viewer.c
+++ ghostscript-viewer/src/viewer.c
@@ -218,20 +218,22 @@ void interpreter_message(GtkGS *gs, gchar *msg, gpointer user_data)
 	debug_print("%s\n", msg);
 }
 
-void nextpage(GhostscriptViewer *ghostscriptviewer)
+void goto_page(GhostscriptViewer *ghostscriptviewer, gint page)
 {
-	gtk_gs_goto_page(GTK_GS(ghostscriptviewer->gs), ghostscriptviewer->page + 1);
+	gtk_gs_goto_page(GTK_GS(ghostscriptviewer->gs), page);
 	ghostscriptviewer->page = gtk_gs_get_current_page(GTK_GS(ghostscriptviewer->gs));
 
 	update_page_buttons(ghostscriptviewer);
 }
 
+void nextpage(GhostscriptViewer *ghostscriptviewer)
+{
+	goto_page(ghostscriptviewer, ghostscriptviewer->page + 1);
+}
+
 void prevpage(GhostscriptViewer *ghostscriptviewer)
 {
-	gtk_gs_goto_page(GTK_GS(ghostscriptviewer->gs), ghostscriptviewer->page - 1);
-	ghostscriptviewer->page = gtk_gs_get_current_page(GTK_GS(ghostscriptviewer->gs));
-
-	update_page_buttons(ghostscriptviewer);
+	goto_page(ghostscriptviewer, ghostscriptviewer->page - 1);
 }
 
 void zoomin(GhostscriptViewer *ghostscriptviewer)
@@ -278,6 +280,16 @@ void on_prev_page_activate(GtkMenuItem *menuitem, GhostscriptViewer *ghostscript
 	prevpage(ghostscriptviewer);
 }
 
+void on_first_page_activate(GtkMenuItem *menuitem, GhostscriptViewer *ghostscriptviewer)
+{
+	goto_page(ghostscriptviewer, 0);
+}
+
+void on_last_page_activate(GtkMenuItem *menuitem, GhostscriptViewer *ghostscriptviewer)
+{
+	goto_page(ghostscriptviewer, ghostscriptviewer->pages);
+}
+
 void on_zoom_in_activate(GtkMenuItem *menuitem, GhostscriptViewer *ghostscriptviewer)
 {
 	zoomin(ghostscriptviewer);
@@ -320,6 +332,8 @@ MimeViewer *ghostscript_viewer_create(void)
 	GtkAccelGroup *popupmenu_accels;
 	GtkWidget *next_page;
 	GtkWidget *prev_page;
+	GtkWidget *first_page;
+	GtkWidget *last_page;
 	GtkWidget *item1;
 	GtkWidget *zoom_in;
 	GtkWidget *zoom_out;
@@ -365,35 +379,78 @@ MimeViewer *ghostscript_viewer_create(void)
 	gtk_frame_set_shadow_type(GTK_FRAME(gsframe), GTK_SHADOW_IN);
 
 	popupmenu = gtk_menu_new();
+	gtk_object_set_data(GTK_OBJECT(popupmenu), "popupmenu", popupmenu);
 	popupmenu_accels =
 	    gtk_menu_ensure_uline_accel_group(GTK_MENU(popupmenu));
 
 	next_page = gtk_menu_item_new_with_label(_("Next Page"));
+	gtk_widget_ref(next_page);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "next_page",
+				 next_page,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(next_page);
 	gtk_container_add(GTK_CONTAINER(popupmenu), next_page);
 
 	prev_page = gtk_menu_item_new_with_label(_("Prev Page"));
+	gtk_widget_ref(prev_page);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "prev_page",
+				 prev_page,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(prev_page);
 	gtk_container_add(GTK_CONTAINER(popupmenu), prev_page);
 
+	first_page = gtk_menu_item_new_with_label(_("First Page"));
+	gtk_widget_ref(first_page);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "first_page",
+				 first_page,
+				 (GtkDestroyNotify) gtk_widget_unref);
+	gtk_widget_show(first_page);
+	gtk_container_add(GTK_CONTAINER(popupmenu), first_page);
+
+	last_page = gtk_menu_item_new_with_label(_("Last Page"));
+	gtk_widget_ref(last_page);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "last_page",
+				 last_page,
+				 (GtkDestroyNotify) gtk_widget_unref);
+	gtk_widget_show(last_page);
+	gtk_container_add(GTK_CONTAINER(popupmenu), last_page);
+
 	item1 = gtk_menu_item_new();
+	gtk_widget_ref(item1);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "item1", item1,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(item1);
 	gtk_container_add(GTK_CONTAINER(popupmenu), item1);
 	gtk_widget_set_sensitive(item1, FALSE);
 
 	zoom_in = gtk_menu_item_new_with_label(_("Zoom in"));
+	gtk_widget_ref(zoom_in);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "zoom_in", zoom_in,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(zoom_in);
 	gtk_container_add(GTK_CONTAINER(popupmenu), zoom_in);
 
 	zoom_out = gtk_menu_item_new_with_label(_("Zoom out"));
+	gtk_widget_ref(zoom_out);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "zoom_out",
+				 zoom_out,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(zoom_out);
 	gtk_container_add(GTK_CONTAINER(popupmenu), zoom_out);
 
 	fit_page = gtk_menu_item_new_with_label(_("Fit page"));
+	gtk_widget_ref(fit_page);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "fit_page",
+				 fit_page,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(fit_page);
 	gtk_container_add(GTK_CONTAINER(popupmenu), fit_page);
 
 	fit_width = gtk_menu_item_new_with_label(_("Fit width"));
+	gtk_widget_ref(fit_width);
+	gtk_object_set_data_full(GTK_OBJECT(popupmenu), "fit_width",
+				 fit_width,
+				 (GtkDestroyNotify) gtk_widget_unref);
 	gtk_widget_show(fit_width);
 	gtk_container_add(GTK_CONTAINER(popupmenu), fit_width);
 	/*
@@ -455,6 +512,11 @@ MimeViewer *ghostscript_viewer_create(void)
 			   GTK_SIGNAL_FUNC(on_next_page_activate), ghostscriptviewer);
 	gtk_signal_connect(GTK_OBJECT(prev_page), "activate",
 			   GTK_SIGNAL_FUNC(on_prev_page_activate), ghostscriptviewer);
+	gtk_signal_connect(GTK_OBJECT(first_page), "activate",
+			   GTK_SIGNAL_FUNC(on_first_page_activate), ghostscriptviewer);
+	gtk_signal_connect(GTK_OBJECT(last_page), "activate",
+			   GTK_SIGNAL_FUNC(on_last_page_activate), ghostscriptviewer);
+
 	gtk_signal_connect(GTK_OBJECT(zoom_in), "activate",
 			   GTK_SIGNAL_FUNC(on_zoom_in_activate), ghostscriptviewer);
 	gtk_signal_connect(GTK_OBJECT(zoom_out), "activate",
