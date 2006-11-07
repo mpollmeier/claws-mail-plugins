fab625febdff724b8e1b7059c3051404537dca37
colinler <colinler>
2006-11-07 10:51:09 +0000

    2006-11-07 [paul]	0.10.2cvs1
    	* ChangeLog
    	* configure.ac
    	* debian/README.Debian
    	* debian/changelog
    	* debian/control
    	* debian/copyright
    	* debian/files
    	* debian/rules
    	* src/Makefile.am
    	* src/plugin.c
    		Name change

--- cachesaver/src/plugin.c
+++ cachesaver/src/plugin.c
@@ -1,5 +1,5 @@
 /*
- * CacheSaver plugin for sylpheed-claws
+ * CacheSaver plugin for claws-mail
  * Copyright (C) 2004 Colin Leroy
  *
  * This program is free software; you can redistribute it and/or modify
@@ -51,12 +51,12 @@ gint save_caches(void *data)
 gint plugin_init(gchar **error)
 {
 	if ((sylpheed_get_version() > VERSION_NUMERIC)) {
-		*error = g_strdup("Your Sylpheed-Claws version is newer than the version the plugin was built with");
+		*error = g_strdup("Your Claws Mail version is newer than the version the plugin was built with");
 		return -1;
 	}
 
 	if ((sylpheed_get_version() < MAKE_NUMERIC_VERSION(0, 9, 12, 0))) {
-		*error = g_strdup("Your Sylpheed-Claws version is too old");
+		*error = g_strdup("Your Claws Mail version is too old");
 		return -1;
 	}
 	if (tag == 0)
