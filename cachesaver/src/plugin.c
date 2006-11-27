fc4b97fb140ecc75b39ebc88015e2b52ae4da281
twb <twb>
2006-11-27 08:12:46 +0000

    2006-11-27 [paul]	0.10.2cvs2
    	* debian/README.Debian
    	* src/plugin.c
    		renaming

--- cachesaver/src/plugin.c
+++ cachesaver/src/plugin.c
@@ -50,12 +50,12 @@ gint save_caches(void *data)
 
 gint plugin_init(gchar **error)
 {
-	if ((sylpheed_get_version() > VERSION_NUMERIC)) {
+	if ((claws_get_version() > VERSION_NUMERIC)) {
 		*error = g_strdup("Your Claws Mail version is newer than the version the plugin was built with");
 		return -1;
 	}
 
-	if ((sylpheed_get_version() < MAKE_NUMERIC_VERSION(0, 9, 12, 0))) {
+	if ((claws_get_version() < MAKE_NUMERIC_VERSION(0, 9, 12, 0))) {
 		*error = g_strdup("Your Claws Mail version is too old");
 		return -1;
 	}
