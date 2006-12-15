04468815a0270516cffe40d89d069ecc589b3866
ticho <ticho>
2006-12-15 20:25:54 +0000

    2006-12-15 [ticho]	0.6.5cvs1
    	* configure.ac
    	* src/synce_plugin.c
    		Use check_plugin_version() for version check.

--- synce/src/synce_plugin.c
+++ synce/src/synce_plugin.c
@@ -38,15 +38,9 @@
 gint plugin_init(gchar **error)
 {
   /* version check */
-  if((claws_get_version() > VERSION_NUMERIC)) {
-    *error = g_strdup("Your Claws Mail version is newer than the version "
-		      "the plugin was built with");
-    return -1;
-  }
-  if((claws_get_version() < MAKE_NUMERIC_VERSION(1, 0, 3, 4))) {
-    *error = g_strdup("Your Claws Mail version is too old");
-    return -1;
-  }
+	if(!check_plugin_version(MAKE_NUMERIC_VERSION(2, 6, 1, 41),
+				VERSION_NUMERIC, "SynCE", error))
+		return -1;
 
   synce_gtk_init();
 
