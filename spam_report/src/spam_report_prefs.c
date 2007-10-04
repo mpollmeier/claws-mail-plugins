95d81d7899142843ad4682200fd60a295a04baae
colinler <colinler>
2007-10-04 17:42:47 +0000

    2007-10-04 [colin]	0.2.0cvs6
    	* src/spam_report_prefs.c
    		Verify success on writes

--- spam_report/src/spam_report_prefs.c
+++ spam_report/src/spam_report_prefs.c
@@ -211,6 +211,9 @@ static void save_spamreport_prefs(PrefsPage *page)
           prefs_file_close_revert(pref_file);
           return;
         }
-        fprintf(pref_file->fp, "\n");
-        prefs_file_close(pref_file);
+        if (fprintf(pref_file->fp, "\n") < 0) {
+		FILE_OP_ERROR(rc_file_path, "fprintf");
+		prefs_file_close_revert(pref_file);
+	} else
+	        prefs_file_close(pref_file);
 }
