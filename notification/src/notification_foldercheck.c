ea5d12c5be28aa77df63c9ad6a66a0aa57fad475
wwp <wwp>
2008-07-02 08:45:39 +0000

    2008-07-02 [wwp]	0.17cvs1
    	* src/notification_foldercheck.c
    		Fix compilation with older GTKs.

--- notification/src/notification_foldercheck.c
+++ notification/src/notification_foldercheck.c
@@ -523,8 +523,10 @@ static void foldercheck_create_window(SpecificFolderArrayEntry *entry)
 				  FOLDERCHECK_FOLDERNAME);
   gtk_tree_view_set_rules_hint(GTK_TREE_VIEW(entry->treeview),
 				  prefs_common.use_stripes_everywhere);
+#if GTK_CHECK_VERSION(2,10,0)
   gtk_tree_view_set_enable_tree_lines(GTK_TREE_VIEW(entry->treeview),
 				  prefs_common.enable_dotted_lines);
+#endif
 
   selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(entry->treeview));
   gtk_tree_selection_set_mode(selection, GTK_SELECTION_BROWSE);
