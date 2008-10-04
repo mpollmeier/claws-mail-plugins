f813538667431c9df411ec4a866854e34d6dac61
colinler <colinler>
2008-10-04 13:27:54 +0000

    2008-10-04 [colin]	0.2.13cvs2
    	* src/attachwarner.c
    		Win32: set translation dir

--- attachwarner/src/attachwarner.c
+++ attachwarner/src/attachwarner.c
@@ -280,7 +280,11 @@ gboolean my_before_send_hook(gpointer source, gpointer data)
  */
 gint plugin_init(gchar **error)
 {
-        bindtextdomain(TEXTDOMAIN, LOCALEDIR);
+#ifdef G_OS_UNIX
+	bindtextdomain(TEXTDOMAIN, LOCALEDIR);
+#else
+	bindtextdomain(TEXTDOMAIN, get_locale_dir());
+#endif
 	bind_textdomain_codeset(TEXTDOMAIN, "UTF-8");
 
 	if (!check_plugin_version(MAKE_NUMERIC_VERSION(2,9,2,72),
